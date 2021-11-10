769a12152f3d639c41464731d0a5308e90d15386
Aang Ardiansyah-XD <94035046+Aang021@users.noreply.github.com>
2021-11-10 21:54:18 +0700

    Add files via upload

---
+++ Kiya.py
@@ -0,0 +1,11 @@
+
+# ('MANTAN')
+
+     Nggak ada apa² hanya saya sendirian
+ 
+                      ###SAD###
+     
+Contact me : 089524163441/088207090803     
+Sekian pamit pergi=<"Alay__Anjing")>     
+     
+     
